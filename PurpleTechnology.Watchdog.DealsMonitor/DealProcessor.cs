168130cd436a134fd5f8902246c757cf74823bb9
stefan-zajicek <stefan.zajicek@thermofisher.com>
2023-04-13 00:05:58 +0200

    Stefan Zajicek Solution
    I restricted myself to modifying only the DealsMonitor and Test projects. However, I believe that a more resilient solution would involve separating the producer and consumer into smaller, more ideal container services. I've included a diagram of this design in the DealsMonitor project folder. Nonetheless, I hope that this implementation showcases my programming skills.

---
+++ PurpleTechnology.Watchdog.DealsMonitor/DealProcessor.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Extensions.Logging;
+using PurpleTechnology.Watchdog.DealsMonitor.Config;
+using PurpleTechnology.Watchdog.DealsMonitor.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PurpleTechnology.Watchdog.DealsMonitor
+{
+	public class DealProcessor<TServerId>
+	{
+		//Indexing data by OpenTime and BalanceToVolumeRatio
+		private readonly Dictionary<string, SortedDictionary<long, SortedDictionary<decimal, Deal<TServerId>>>> _indexedDeals = new Dictionary<string, SortedDictionary<long, SortedDictionary<decimal, Deal<TServerId>>>>();
+
+		//Be aware It does not persist data!
+		//For more robust solution would be better to decouple producer/consumer into microservice and keep small SQL DB (e.g. SqlLite) for each replica of DealProcessor service
+		public async Task Watch(IAsyncEnumerable<Deal<TServerId>> data,
+								ILogger logger,
+								DealsMonitorConfig dealsMonitorconfig,
+								CancellationToken stoppingToken
+			)
+		{
+
+			await foreach (var deal in data) {
+				 stoppingToken.ThrowIfCancellationRequested();
+
+				if (!_indexedDeals.ContainsKey(deal.Symbol)) {
+					_indexedDeals.Add(deal.Symbol, CreateIndexedData(deal));
+					continue;
+				}
+				var openTimeMax = deal.OpenTime + dealsMonitorconfig.OpenTimeDelta.TotalSeconds;
+				var openTimeMin = deal.OpenTime - dealsMonitorconfig.OpenTimeDelta.TotalSeconds;
+				var volumeToBalanceMax = deal.VolumeToBalanceRate + dealsMonitorconfig.VolumeToBalanceRatio;
+				var volumeToBalanceMin = deal.VolumeToBalanceRate - dealsMonitorconfig.VolumeToBalanceRatio;
+				var firstRelevantOpenTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds() - ((long)dealsMonitorconfig.CacheTresholdTimeDelta.TotalSeconds);
+				var indexedDataForSymbol = _indexedDeals[deal.Symbol];
+
+				CleanFromExpiredData(firstRelevantOpenTime, indexedDataForSymbol);
+
+				Deal<TServerId> firstSimilarDeal = null;
+
+				foreach (var openTimeIndex in indexedDataForSymbol) { 
+					
+					if (openTimeIndex.Key < openTimeMin) continue;
+					if (openTimeIndex.Key > openTimeMax) break;
+
+					foreach (var volumeToBalanceIndex in indexedDataForSymbol[openTimeIndex.Key]) {
+						
+						if (volumeToBalanceIndex.Key <= volumeToBalanceMin) continue;
+						if (volumeToBalanceIndex.Key >= volumeToBalanceMax) break;
+
+						firstSimilarDeal = indexedDataForSymbol[openTimeIndex.Key][volumeToBalanceIndex.Key];
+						break;
+					}
+
+					if (!(firstSimilarDeal is null)) {
+						logger.LogWarning($"Similar order found. Order {deal.Order} on server {deal.ServerId}.");
+						break;
+					}
+				};
+				TryToAddDealToIndexedData(indexedDataForSymbol, deal);
+			}
+		}
+
+		private void CleanFromExpiredData(long firstRelevantOpenTime, SortedDictionary<long, SortedDictionary<decimal, Deal<TServerId>>> indexedDataForSymbol)
+		{
+			var entriesToDelete = new List<long>();
+			foreach (var openTimeIndex in indexedDataForSymbol) {
+				
+				if(openTimeIndex.Key > firstRelevantOpenTime) break;
+				entriesToDelete.Add(openTimeIndex.Key);
+			}
+			entriesToDelete.ForEach(e => indexedDataForSymbol.Remove(e));
+		}
+
+		private void TryToAddDealToIndexedData(SortedDictionary<long, SortedDictionary<decimal, Deal<TServerId>>> indexedDataForSymbol, Deal<TServerId> deal)
+		{
+			if (!indexedDataForSymbol.ContainsKey(deal.OpenTime)) {
+
+				var volumeToBalanceIndex = new SortedDictionary<decimal, Deal<TServerId>> ();
+				volumeToBalanceIndex.Add(deal.VolumeToBalanceRate, deal);
+				indexedDataForSymbol.Add(deal.OpenTime, volumeToBalanceIndex);
+				return;
+			}
+
+			var indexedDataOpenTime = indexedDataForSymbol[deal.OpenTime];
+
+			if (!indexedDataOpenTime.ContainsKey(deal.VolumeToBalanceRate)) {
+
+				indexedDataOpenTime.Add(deal.VolumeToBalanceRate, deal);
+
+			}
+		}
+
+		private SortedDictionary<long, SortedDictionary<decimal, Deal<TServerId>>> CreateIndexedData(Deal<TServerId> deal)
+		{
+			var volumeToBalanceIndex = new SortedDictionary<decimal, Deal<TServerId>> ();
+			volumeToBalanceIndex.Add(deal.VolumeToBalanceRate, deal);
+			var openTimeIndex = new SortedDictionary<long, SortedDictionary<decimal, Deal<TServerId>>>();
+			openTimeIndex.Add(deal.OpenTime, volumeToBalanceIndex);
+			return openTimeIndex;
+		}
+	}
+}
--- PurpleTechnology.Watchdog.DealsMonitor/DealsMonitor.cs
+++ PurpleTechnology.Watchdog.DealsMonitor/DealsMonitor.cs
@@ -1,24 +1,116 @@
 ﻿using Microsoft.Extensions.Logging;
+using PurpleTechnology.MT5Wrapper.Abstractions;
+using PurpleTechnology.MT5Wrapper.Data;
+using PurpleTechnology.MT5Wrapper.Events;
 using PurpleTechnology.Watchdog.Abstractions;
+using PurpleTechnology.Watchdog.Config;
+using PurpleTechnology.Watchdog.DealsMonitor.Config;
+using PurpleTechnology.Watchdog.DealsMonitor.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace PurpleTechnology.Watchdog.DealsMonitor
 {
+	//More resilient solution can be done by decouple producer/consumer into microservices
+	//for purpuse of this the assignment I just demonstrate parellel processing and using complex datastructure which 
+	//mimic table indexing
 	public class DealsMonitor<TServerId> : IMonitor
 	{
 		private readonly ILogger<DealsMonitor<TServerId>> _logger;
+		private readonly DealsMonitorConfig _dealsMonitorConfig;
+		private readonly IMT5ApiFactory<TServerId> _iMT5ApiFactory;
+		private readonly ServersConfig<TServerId> _serversConfig;
+		private readonly Dictionary<MT5DealAction, Channel<Deal<TServerId>>> _channels;
+		private readonly Dictionary<TServerId, (IMT5Api<TServerId>, IMT5Api<TServerId>)> _mt5Apis = new Dictionary<TServerId, (IMT5Api<TServerId>, IMT5Api<TServerId>)>();
 
-		public DealsMonitor(ILogger<DealsMonitor<TServerId>> logger)
+		public DealsMonitor(ILogger<DealsMonitor<TServerId>> logger,
+			DealsMonitorConfig dealsMonitorConfig,
+			ServersConfig<TServerId> serversConfig,
+			IMT5ApiFactory<TServerId> iMT5ApiFactory
+			)
 		{
 			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			_dealsMonitorConfig = dealsMonitorConfig ?? throw new ArgumentNullException(nameof(dealsMonitorConfig));
+			_serversConfig = serversConfig ?? throw new ArgumentNullException(nameof(serversConfig));
+			_iMT5ApiFactory = iMT5ApiFactory ?? throw new ArgumentNullException(nameof(iMT5ApiFactory));
+			
+			//Depend on desire level of parallelism key can be easily modified e.g. action + symbol
+			_channels = new Dictionary<MT5DealAction, Channel<Deal<TServerId>>>() {
+				{MT5DealAction.Sell, Channel.CreateBounded<Deal<TServerId>>(100)},
+				{MT5DealAction.Buy, Channel.CreateBounded<Deal<TServerId>>(100)}
+			};
 		}
 
-		public Task StartMonitoring(CancellationToken stoppingToken)
+		public async void WriteToChannel(object source, DealEventArgs args)
 		{
-			// TODO: implement suspicious deals monitoring
-			return Task.CompletedTask;
+			var mt5Deal = args.Deal;
+
+			if (mt5Deal.Action == MT5DealAction.Sell) {
+
+				var server = (IMT5Api<TServerId>) source;
+				Deal<TServerId> monitorDeal = Mt5DealToMonitorDeal(server.ServerId, mt5Deal);
+				await _channels[MT5DealAction.Sell].Writer.WriteAsync(monitorDeal);
+
+			}
+			else if (mt5Deal.Action == MT5DealAction.Buy) {
+
+				var server = (IMT5Api<TServerId>) source;
+				Deal<TServerId> monitorDeal = Mt5DealToMonitorDeal(server.ServerId, mt5Deal);
+				await _channels[MT5DealAction.Buy].Writer.WriteAsync(monitorDeal);
+			}
+		}
+
+		private Deal<TServerId> Mt5DealToMonitorDeal(TServerId mt5Api, MT5Deal mt5Deal)
+		{
+			var userBalance = _mt5Apis[mt5Api].Item2.GetUserBalance(mt5Deal.Login);
+			var volumeToBalanceRatio = mt5Deal.Volume/userBalance;
+			var monitorDeal = new Deal<TServerId>{
+				Action = mt5Deal.Action,
+				Login = mt5Deal.Login,
+				OpenTime = mt5Deal.OpenTime,
+				Order = mt5Deal.Order,
+				Symbol = mt5Deal.Symbol,
+				VolumeToBalanceRate = volumeToBalanceRatio,
+				ServerId = mt5Api
+				
+			};
+			return monitorDeal;
+		}
+
+		//Can be extended to higher level of parallelism, by add new channels for Symbols
+		//Be aware It does not persist data! In that case should be use message queue instead of channels and decople consumer/producer in microservises
+		public async Task StartMonitoring(CancellationToken stoppingToken)
+		{
+			try {
+
+				foreach (var serverConfig in _serversConfig.MT5ServersConfig) {
+					var mt5ApiEventSubscription = _iMT5ApiFactory.Create();
+					mt5ApiEventSubscription.Connect(serverConfig);
+
+					var mt5ApiBalanceCheck = _iMT5ApiFactory.Create();
+					mt5ApiBalanceCheck.Connect(serverConfig);
+
+					_mt5Apis.Add(serverConfig.ServerId, (mt5ApiEventSubscription, mt5ApiBalanceCheck));
+
+					mt5ApiEventSubscription.DealAddEvent += WriteToChannel;
+				}
+
+				List<Task> extractionTask = new List<Task>();
+
+				extractionTask.Add(new DealProcessor<TServerId>().Watch(_channels[MT5DealAction.Sell].Reader.ReadAllAsync(), _logger, _dealsMonitorConfig, stoppingToken));
+				extractionTask.Add(new DealProcessor<TServerId>().Watch(_channels[MT5DealAction.Buy].Reader.ReadAllAsync(), _logger, _dealsMonitorConfig, stoppingToken));
+				await Task.WhenAll(extractionTask).ConfigureAwait(false);
+			}
+			finally {
+				_mt5Apis.ToList().ForEach(keyValuePair => {
+					keyValuePair.Value.Item1.Dispose();
+					keyValuePair.Value.Item2.Dispose();
+				});
+			}
 		}
 	}
 }
